b20f57610398f2866eb9c4a94767e1521328db0a
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2022-10-29 00:55:12 +0100

    Update settings.py

--- Otp_projectAPI/settings.py
+++ Otp_projectAPI/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o#w8g5rqux%#@%mdtf3ldv-ucfe3aelj)s9k1_9i^m=bev=aqq'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
